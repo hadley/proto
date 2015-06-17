82c0d47a55c0caf0fa7003e004d08ceeb1fbb092
hadley <h.wickham@gmail.com>
2015-06-17 09:01:32 -0500

    Reformat code

--- R/graph.proto.R
+++ R/graph.proto.R
@@ -1,39 +1,44 @@
 name.proto <- function(., envir = parent.frame()) {
-   stopifnot(is.environment(.) || 
+  stopifnot(is.environment(.) ||
       (is.character(.) && is.environment(get(., envir))))
-   if (is.environment(.)) {
-      if (exists("..Name", ., inherits = FALSE)) .$..Name
-      else {
-         L <- unlist(eapply(envir, identical, .))
-         if (any(L)) names(L[L])[1]
-         else gsub("^.* |>$", "", capture.output(print.default(.))[[1]])
-      }
-   } else {
-      e <- get(., envir)
-      if (exists("..Name", e, inherits = FALSE)) e$..Name
-      else .
-   }
+  if (is.environment(.)) {
+    if (exists("..Name", ., inherits = FALSE))
+      .$..Name
+    else {
+      L <- unlist(eapply(envir, identical, .))
+      if (any(L))
+        names(L[L])[1]
+      else
+        gsub("^.* |>$", "", capture.output(print.default(.))[[1]])
+    }
+  } else {
+    e <- get(., envir)
+    if (exists("..Name", e, inherits = FALSE))
+      e$..Name
+    else
+      .
+  }
 }
-      
+
 
 
 #' Create a graph of proto objects
-#' 
+#'
 #' Creates a graph of the parent/child relationships among a set of proto
 #' objects.
-#' 
+#'
 #' This function is used to create a graph in the sense of the \code{"graph"}
 #' package out of the parent child relationships of proto objects.  All
 #' \code{"proto"} objects in \code{"e"} and their immediate parents are placed
 #' in the graph.
-#' 
+#'
 #' The objects are labelled using their \code{"..Name"} component.  If there is
 #' no \code{"..Name"} component then their variable name in \code{"e"} is used.
 #' In the case of a parent that is not in \code{"e"}, an internally generated
 #' name will be used if the parent has no \code{"..Name"} component.  If two
 #' \code{"proto"} objects to be graphed have the same name the result is
 #' undefined.
-#' 
+#'
 #' @aliases graph.proto name.proto
 #' @param e A proto object or an environment whose proto objects will be
 #' graphed.
@@ -50,7 +55,7 @@ name.proto <- function(., envir = parent.frame()) {
 #' LD_LIBRARY_PATH=/path/to/graphviz/libs}.
 #' @keywords programming
 #' @examples
-#' 
+#'
 #' \dontrun{
 #' a <- proto()
 #' b <- a$proto()
@@ -61,32 +66,33 @@ name.proto <- function(., envir = parent.frame()) {
 #' g <- graph.proto(g = new("graphNEL")) # undirected
 #' plot(g)
 #' g <- graph.proto()
-#' attrs <- list(node = list(fillcolor = "lightgreen"), 
+#' attrs <- list(node = list(fillcolor = "lightgreen"),
 #'               edge = list(color = "cyan"),
 #'       	graph = list(rankdir = "BT"))
 #' plot(graph.proto(), attrs = attrs) # specify plot attributes
 #' }
-#' 
-graph.proto <- function(e,
-   g = new("graphNEL", edgemode = "directed"), child.to.parent = TRUE) {
-   if (missing(e)) e <- 
-      if (exists(".that")) get(".that") else parent.frame()
-   if ( suppressWarnings(! require(graph) || ! require(Rgraphviz)) )
-      stop("Error: packages graph and Rgraphviz must be available for loading")
-   # add node if its not already in g
-   addNode. <- function(x, g) if (x %in% nodes(g)) g else addNode(x, g)
-   # add edge between nodes adding nodes too if not already in g
-   addEdge. <- function(x, y, g) {
-      g <- addNode.(x,g); g <- addNode.(y, g)
-      addEdge(x, y, g, 1)
-   }
-   nn <- unlist(eapply(e, is.proto))
-   for(x in names(nn[nn]))
-      g <- if (child.to.parent)
-         addEdge.(name.proto(x,e), name.proto(get(x,e)$parent.env(), e), g)
-      else
-         addEdge.(name.proto(get(x,e)$parent.env(), e), name.proto(x, e), g)
-   g
+#'
+graph.proto <- function(e, g = new("graphNEL", edgemode = "directed"),
+                        child.to.parent = TRUE) {
+  if (missing(e))
+    e <- if (exists(".that")) get(".that") else parent.frame()
+
+  if (suppressWarnings(!require(graph) || !require(Rgraphviz)))
+    stop("Error: packages graph and Rgraphviz must be available for loading")
+  # add node if its not already in g
+  addNode. <- function(x, g) if (x %in% nodes(g)) g else addNode(x, g)
+  # add edge between nodes adding nodes too if not already in g
+  addEdge. <- function(x, y, g) {
+    g <- addNode.(x,g); g <- addNode.(y, g)
+    addEdge(x, y, g, 1)
+  }
+  nn <- unlist(eapply(e, is.proto))
+  for (x in names(nn[nn]))
+    g <- if (child.to.parent)
+      addEdge.(name.proto(x,e), name.proto(get(x,e)$parent.env(), e), g)
+  else
+    addEdge.(name.proto(get(x,e)$parent.env(), e), name.proto(x, e), g)
+  g
 }
 
 ### test
@@ -99,8 +105,7 @@ graph.proto <- function(e,
 # g <- graph.proto(new("graphNEL")) # undirected
 # plot(g)
 # g <- graph.proto()
-# attrs <- list(node = list(fillcolor = "lightgreen"), 
+# attrs <- list(node = list(fillcolor = "lightgreen"),
 #               edge = list(color = "cyan"),
 #		graph = list(rankdir = "BT"))
 # plot(graph.proto(), attrs) # specify plot attributes
-
--- R/proto.R
+++ R/proto.R
@@ -299,46 +299,61 @@ NULL
 #' as.list(q$L)
 #'
 #'
-proto <- function (. = parent.env(envir), expr = {}, envir =
-		new.env(parent = parent.frame()), ..., funEnvir = envir) {
-    parent.env(envir) <- .
-    as.proto.environment(envir)  # must do this before eval(...)
-    # moved eval after for so that ... always done first
-    # eval(substitute(eval(quote({ expr }))), envir)
-    dots <- list(...); names <- names(dots)
-    for (i in seq(length = length(dots))) {
-      assign(names[i], dots[[i]], env = envir)
-      if (!identical(funEnvir, FALSE) && is.function(dots[[i]]))
-        environment(envir[[names[i]]]) <- funEnvir
-    }
-    eval(substitute(eval(quote({ expr }))), envir)
-    if (length(dots)) as.proto.environment(envir) else envir
+proto <- function (. = parent.env(envir), expr = {},
+                   envir = new.env(parent = parent.frame()), ...,
+                   funEnvir = envir) {
+  parent.env(envir) <- .
+  as.proto.environment(envir)  # must do this before eval(...)
+  # moved eval after for so that ... always done first
+  # eval(substitute(eval(quote({ expr }))), envir)
+  dots <- list(...); names <- names(dots)
+  for (i in seq(length = length(dots))) {
+    assign(names[i], dots[[i]], env = envir)
+    if (!identical(funEnvir, FALSE) && is.function(dots[[i]]))
+      environment(envir[[names[i]]]) <- funEnvir
+  }
+  eval(substitute(eval(quote({
+    expr
+  }))), envir)
+  if (length(dots))
+    as.proto.environment(envir)
+  else
+    envir
 }
 
-as.proto <- function(x, ...) UseMethod("as.proto")
-as.proto.environment <- function(x, ...) {
-	assign(".that", x, env = x)
-	assign(".super", parent.env(x), env = x)
-	structure(x, class = c("proto", "environment"))
+as.proto <- function(x, ...) {
+  UseMethod("as.proto")
 }
-as.proto.proto <- function(x, ...) x
+
+as.proto.environment <- function(x, ...) {
+  assign(".that", x, env = x)
+  assign(".super", parent.env(x), env = x)
+  structure(x, class = c("proto", "environment"))
+}
+
+as.proto.proto <- function(x, ...) {
+  x
+}
+
 as.proto.list <- function(x, envir, parent, all.names = FALSE, ...,
-	funEnvir = envir, SELECT = function(x) TRUE) {
-       if (missing(envir)) {
-		if (missing(parent)) parent <- parent.frame()
-		envir <- if (is.proto(parent))
-			parent$proto(...)
-		else
-			proto(parent, ...)
-       }
-       for(s in names(x))
-          if (SELECT(x[[s]])) {
-             assign(s, x[[s]], env = envir)
-             if (is.function(x[[s]]) && !identical(funEnvir, FALSE))
-		environment(envir[[s]]) <- funEnvir
-          }
-       if (!missing(parent)) parent.env(envir) <- parent
-       as.proto.environment(envir)  # force refresh of .that and .super
+                          funEnvir = envir, SELECT = function(x) TRUE) {
+  if (missing(envir)) {
+    if (missing(parent))
+      parent <- parent.frame()
+    envir <- if (is.proto(parent))
+      parent$proto(...)
+    else
+      proto(parent, ...)
+  }
+  for (s in names(x))
+    if (SELECT(x[[s]])) {
+      assign(s, x[[s]], env = envir)
+      if (is.function(x[[s]]) && !identical(funEnvir, FALSE))
+        environment(envir[[s]]) <- funEnvir
+    }
+  if (!missing(parent))
+    parent.env(envir) <- parent
+  as.proto.environment(envir)  # force refresh of .that and .super
 }
 
 # "$.proto" <- function(this, x) {
@@ -356,30 +371,31 @@ as.proto.list <- function(x, envir, parent, all.names = FALSE, ...,
 # }
 
 "$<-.proto" <- function(this,s,value) {
-        if (s == ".super") parent.env(this) <- value
-	if (is.function(value))  environment(value) <- this
-	this[[as.character(substitute(s))]] <- value
-	this
+  if (s == ".super")
+    parent.env(this) <- value
+  if (is.function(value))
+    environment(value) <- this
+  this[[as.character(substitute(s))]] <- value
+  this
 }
 
 is.proto <- function(x) inherits(x, "proto")
-isnot.function <- function(x) ! is.function(x)
+isnot.function <- function(x) !is.function(x)
 
-"$.proto" <-
-function (this, x, args) {
-   inh <- substr(x, 1, 2) != ".."
-   p <- parent.frame()
-   res <- get(x, env = this, inherits = inh)
-   is.function <- is.function(res)
-   is.that <- match(deparse(substitute(this)), c(".that", ".super"),
-       nomatch = 0)
-   if (is.function && !is.that) {
-       res <- function(...) get(x, env = this, inherits = inh)(this, ...)
-       class(res) <- c("instantiatedProtoMethod", "function")
-       attr(res, "this") <- this
-       if (!missing(args)) res <- do.call(res, args, envir = p)
-   }
-   res
+"$.proto" <- function (this, x, args) {
+  inh <- substr(x, 1, 2) != ".."
+  p <- parent.frame()
+  res <- get(x, env = this, inherits = inh)
+  is.function <- is.function(res)
+  is.that <- match(deparse(substitute(this)), c(".that", ".super"), nomatch = 0)
+  if (is.function && !is.that) {
+    res <- function(...) get(x, env = this, inherits = inh)(this, ...)
+    class(res) <- c("instantiatedProtoMethod", "function")
+    attr(res, "this") <- this
+    if (!missing(args))
+      res <- do.call(res, args, envir = p)
+  }
+  res
 }
 
 
@@ -393,15 +409,18 @@ print.instantiatedProtoMethod <- function(x, ...) {
 }
 
 # modified from Tom Short's original
-str.proto <- function(object, max.level = 1, nest.lev = 0, indent.str =
-   paste(rep.int(" ", max(0, nest.lev + 1)), collapse = ".."), ...) {
- cat("proto", name.proto(object), "\n")
- Lines <- capture.output(str(as.list(object), max.level = max.level,
-    nest.lev = nest.lev, ...))[-1]
- for(s in Lines) cat(s, "\n")
- if (is.proto(parent.env(object))) {
-   cat(indent.str, "parent: ", sep = "")
-   str(parent.env(object), nest.lev = nest.lev + 1, ...)
- }
+str.proto <- function(object, max.level = 1, nest.lev = 0,
+                      indent.str = paste(rep.int(" ", max(0, nest.lev + 1)), collapse = ".."),
+                      ...) {
+  cat("proto", name.proto(object), "\n")
+  Lines <- capture.output(str(
+    as.list(object), max.level = max.level,
+    nest.lev = nest.lev, ...
+  ))[-1]
+  for (s in Lines)
+    cat(s, "\n")
+  if (is.proto(parent.env(object))) {
+    cat(indent.str, "parent: ", sep = "")
+    str(parent.env(object), nest.lev = nest.lev + 1, ...)
+  }
 }
-
