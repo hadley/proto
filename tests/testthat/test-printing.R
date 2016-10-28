74c1b04e21675ed039134478a63f292a6ed23a0a
hadley <h.wickham@gmail.com>
2016-10-28 13:02:42 -0500

    Work around test failure
    Might be bug in testthat?

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -2,10 +2,12 @@ context("printing")
 
 test_that("can override printing with proto_print", {
   x <- proto(proto_print = function(.) 10)
-  expect_output(print(x), "10")
+  out <- capture.output(print(x))[[1]]
+  expect_match(out, "10")
 })
 
 test_that("default object prints as environment", {
   x <- proto()
-  expect_output(print(x), "environment")
+  out <- capture.output(print(x))[[1]]
+  expect_match(out, "environment")
 })
